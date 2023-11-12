39e6a03c69e4705c45630ec11793096b608a1fd3
vitukjkk <vittorcolom@gmail.com>
2023-11-12 19:01:35 -0300

    a

---
+++ modulos/general/cores.py
@@ -0,0 +1,39 @@
+# cores no python
+
+# \033[0;33;44m
+
+# \033[STYLE;TEXT;BACKm
+
+# style: 
+'''
+0
+1 - negrito
+4 - sublinhado
+7 - inversa
+'''
+
+# text:
+'''
+30 - branco
+31 - vermelho
+32 - verde
+33 - amarelo
+34 - azul
+35 - roxo
+36 - ciano
+37 - cinza
+'''
+
+# back:
+'''
+40 - branco
+41 - vermelho
+42 - verde
+43 - amarelo
+44 - azul
+45 - roxo
+46 - ciano
+47 - cinza
+'''
+
+print('\033[1;31;43mOlá, Mundo!\033[m')
---
+++ modulos/general/if.py
@@ -0,0 +1,7 @@
+number = int(input("insira um numero"))
+
+if number > 1:
+    print("e maior que 1")
+else:
+    print("nao e maior que 1")
+print("voce chegou ao fim")
